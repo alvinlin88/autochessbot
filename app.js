c6581a5042096811d0a99321a0392c15ae10403e
Nhat Duong <vinthian@gmail.com>
2019-02-12 21:48:11 -0500

    Missed one.

--- app.js
+++ app.js
@@ -1682,7 +1682,7 @@ discordClient.on('message', message => {
                                     if (rank.score !== null) {
                                         MMRStr =  " MMR is: `" + rank.score + "`.";
                                     }
-                                    sendChannelandMention(message.channel.id, message.author.id, "Current rank for <@" + getRankUser.discord + "> is: `" + getRankString(rank.mmr_level) + "`." + MMRStr);
+                                    sendChannelandMention(message.channel.id, message.author.id, "Current rank for <@" + getRankUser.discord + "> is: " + getRankString(rank.mmr_level) + "." + MMRStr);
 
                                     if (leagueLobbies.includes(message.channel.name)) {
                                         deleteMessage(message);
