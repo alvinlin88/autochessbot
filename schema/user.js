1e0a46ef5b30e8ac7a9667128762610eb2380054
Ziqiang Shen <ziqiang.shen2013@gmail.com>
2019-02-16 18:22:33 -0800

    Models need to be defined in one file so that you can reference other models to define foreign keys without introducing a cyclic dependency.

--- schema/user.js
+++ schema/user.js
@@ -1,48 +1,5 @@
-const Sequelize = require('sequelize');
-const dbInstance = require('./db.js');
-const Op = Sequelize.Op;
-
-const User = dbInstance.define('user', {
-    discord: {
-        type: Sequelize.TEXT,
-        unique: true,
-        allowNull: false,
-    },
-    steam: {
-        type: Sequelize.TEXT,
-        // unique: true, // might be bad idea to enforce this (others might steal steam_id without verification)
-        allowNull: true,
-    },
-    rank: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-    },
-    // unused, future proofing database
-    score: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-    },
-    games_played: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-    },
-    steamLinkToken: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-    },
-    validated: {
-        type: Sequelize.BOOLEAN,
-        allowNull: true,
-    },
-    // last_played: {
-    //
-    // }
-    // preferredregions: {
-    //
-    // }
-});
-
-User.sync();
+const Op = require('sequelize').Op;
+const User = require('./models.js').User;
 
 const userUtil = {
     findByDiscord: function (discord) {
