8a5f517d6575a68a192bb6693c0ff750aa5dd6df
risenforces <risenforces@gmail.com>
2019-02-22 16:11:48 +0500

    add LeaguesAPI

---
+++ helpers/LeaguesAPI/index.js
@@ -0,0 +1,59 @@
+const { leagues, regions, requirements } = require("../../leagues-config")
+
+const leaguesChannels = []
+
+const leaguesToChannels = {}
+const channelsToLeagues = {}
+
+leagues.forEach(league => {
+  leaguesToChannels[league] = []
+
+  const basename = league + "-lobbies"
+  leaguesChannels.push(basename)
+
+  leaguesToChannels[league].push(basename)
+  channelsToLeagues[basename] = league
+
+  regions.forEach(region => {
+    const name = league + "-lobbies-" + region
+    leaguesChannels.push(name)
+
+    leaguesToChannels[league].push(name)
+    channelsToLeagues[name] = league
+  })
+})
+
+const get = (obj, key) => {
+  const lcKey = key.toLowerCase()
+
+  if (!obj[lcKey]) throw new Error(`Cannot find key "${lcKey}" in object`)
+
+  return obj[lcKey]
+}
+
+const getLeagues = () => leagues
+const getRegions = () => regions
+const getRequirements = () => requirements
+
+const getAllLeaguesChannels = () => leaguesChannels
+
+const getChannelsFromLeague = league => get(leaguesToChannels, league)
+const getLeagueFromChannel = channel => get(channelsToLeagues, channel)
+
+const getLeagueRequirement = league => get(requirements, league)
+const getChannelRequirement = channel =>
+  getLeagueRequirement(getLeagueFromChannel(channel))
+
+module.exports = {
+  getLeagues,
+  getRegions,
+  getRequirements,
+
+  getAllLeaguesChannels,
+
+  getChannelsFromLeague,
+  getLeagueFromChannel,
+
+  getLeagueRequirement,
+  getChannelRequirement
+}
---
+++ leagues-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  leagues: [
+    "royalty",
+    "master",
+    "expert",
+    "advanced",
+    "intermediate",
+    "beginner"
+  ],
+  regions: ["naw", "nae", "euw", "eue", "ru", "sea", "oce", "sa"],
+  requirements: {
+    royalty: 37,
+    master: 32,
+    expert: 28,
+    advanced: 24,
+    intermediate: 19,
+    beginner: 1
+  }
+}
