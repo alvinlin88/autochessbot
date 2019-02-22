5a8374208e7286a415eaa820a7061123d6d6d086
risenforces <risenforces@gmail.com>
2019-02-23 02:05:15 +0500

    forgot to add this

--- verifyapp/verifyapp.js
+++ verifyapp/verifyapp.js
@@ -12,6 +12,7 @@ app.use(cookieParser());
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(session({secret: 'tony numba wan', resave: false, saveUninitialized: false}));
+app.use('/assets', express.static(__dirname + "/assets"))
 
 const config = require("./config");
 const CLIENT_ID = config.discord_client_id;
