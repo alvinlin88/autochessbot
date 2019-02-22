38c5062e3733a5c1fa8468cb22a180a521453d5f
risenforces <risenforces@gmail.com>
2019-02-22 16:11:38 +0500

    add LobbiesAPI

---
+++ helpers/LobbiesAPI/LobbiesAPI.js
@@ -0,0 +1,3 @@
+const LobbiesAPIClass = require("./LobbiesAPIClass")
+
+module.exports = new LobbiesAPIClass()
---
+++ helpers/LobbiesAPI/index.js
@@ -0,0 +1,3 @@
+const LobbiesAPI = require("./LobbiesAPI")
+
+module.exports = LobbiesAPI
