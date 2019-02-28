dea14dff939ee2ba398d1afb162cc1a9297a30df
Nhat Duong <vinthian@gmail.com>
2019-02-27 22:19:42 -0500

    Use black queen.

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function getRank(rank) {
     if (rank >= (10 + 9) && rank < (10 + 9 + 9)) { return {icon: "♝", name: "Bishop", level: (rank - 9 - 9).toString()}; }
     if (rank >= (10 + 9 + 9) && rank < (10 + 9 + 9 + 9)) { return {icon: "♜", name: "Rook", level: (rank - 9 - 9 - 9).toString()}; }
     if (rank >= (10 + 9 + 9 + 9) && rank < (10 + 9 + 9 + 9 + 1)) { return {icon: "♚", name: "King"}; }
-    if (rank >= (10 + 9 + 9 + 9 + 1)) { return {icon: "♕", name: "Queen"}; }
+    if (rank >= (10 + 9 + 9 + 9 + 1)) { return {icon: "♛", name: "Queen"}; }
     // if (rank >= (10 + 9 + 9 + 9) && rank < (10 + 9 + 9 + 9 + 1)) { return "King-" + (rank - 9 - 9 - 9 - 9).toString(); }
     // if (rank >= (10 + 9 + 9 + 9 + 1)) { return "Queen-" + (rank - 9 - 9 - 9 - 9 - 1).toString(); }
     return "ERROR";
