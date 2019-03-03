14746ea4db1b5b0a7202f55ad8286fe8e0566a5d
Nhat Duong <vinthian@gmail.com>
2019-03-03 18:43:42 -0500

    add channel link to rate limit

--- app.js
+++ app.js
@@ -368,8 +368,12 @@ for(let i = 0; i < config.discord_tokens.length; i++) {
     discordClients[i].on('error', logger.error);
     discordClients[i].on('message', message => handleMsg(message, discordClients[i], discordUtils[i]));
     discordClients[i].on('rateLimit', r => {
+        let channel = "";
+        if (r.path.includes("channel")) {
+            channel = " <#" + r.path.match("[0-9]")[0] + ">";
+        }
         console.log(discordClients[i].user.tag + ": RATE LIMITED " + r.requestLimit + " " + r.timeDifference + "ms " + r.method + " " + r.path);
-        discordUtils[i].sendChannel(discordClients[i].channels.find(c => c.name === "chessbot-warnings").id, discordClients[i].user.tag + ": RATE LIMITED " + r.requestLimit + " " + r.timeDifference + "ms " + r.method + " " + r.path);
+        discordUtils[i].sendChannel(discordClients[i].channels.find(c => c.name === "chessbot-warnings").id, discordClients[i].user.tag + ": RATE LIMITED " + r.requestLimit + " " + r.timeDifference + "ms " + r.method + " " + r.path + channel);
     })
 }
 
