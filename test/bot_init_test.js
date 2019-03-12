a1bd4494b93ff6fae07e22357c0bae3be23337b0
Nhat Duong <vinthian@gmail.com>
2019-03-12 02:30:55 -0400

    Fix test case.

--- test/bot_init_test.js
+++ test/bot_init_test.js
@@ -4,14 +4,14 @@ const Lobbies = require("../lobbies.js");
 const config = require("../config");
 
 describe('BotInitializationTest', function() {
-        it('should init lobbies', function() {
-            const lobbies = new Lobbies();
-            lobbies.restoreLobbies();
-            config.server_ids.forEach(serverID => {
-                config.leagueRoles.forEach(leagueRole => {
-                    let lobbiesInChannel = lobbies.getLobbiesInChannel(serverID, config.leagueToLobbiesPrefix[leagueRole]);
-                    expect(lobbiesInChannel).to.not.be.null;
-                });
+    it('should init lobbies', function() {
+        const lobbies = new Lobbies();
+        lobbies.restoreLobbies();
+        Object.keys(config.server_config).forEach(serverID => {
+            config.leagueRoles.forEach(leagueRole => {
+                let lobbiesInChannel = lobbies.getLobbiesInChannel(serverID, config.leagueToLobbiesPrefix[leagueRole]);
+                expect(lobbiesInChannel).to.not.be.null;
             });
         });
+    });
 });
